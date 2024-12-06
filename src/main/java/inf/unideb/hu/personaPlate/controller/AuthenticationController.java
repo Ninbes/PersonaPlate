643deee7bc6c149b3660e157478b8307892d2c62
Papp Bence (Contractor) <bence-mate.papp@capgemini.com>
2024-12-06 10:49:24 +0100

    Initial ng project creation

--- src/main/java/inf/unideb/hu/personaPlate/controller/AuthenticationController.java
+++ src/main/java/inf/unideb/hu/personaPlate/controller/AuthenticationController.java
@@ -34,14 +34,10 @@ public class AuthenticationController {
     @DeleteMapping("/delete/{userId}")
     public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
         UserEntity authenticatedUser = userService.getAuthenticatedUser();
-
-        // Check if the authenticated user is the same as the user trying to delete their account
         if (authenticatedUser.getId().equals(userId)) {
             authenticationService.deleteUser(userId);
             return ResponseEntity.ok("User deleted successfully");
         }
-
-        // Return a forbidden response if the user is not authorized to delete the account
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You cannot delete another user's account");
     }
 }
